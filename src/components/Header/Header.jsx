f38f8de9e946b39baca4d9ee8f9fb50424e1b5ac
Joseph Madras <josephkabemba77@gmail.com>
2021-12-27 13:48:22 +0100

    style: set dark mode theme in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,37 @@
+import { useContext } from "react";
 import { Switch } from "../../components";
+import { ThemeContext } from "../../contexts/theme";
 import headerStyle from "./Header.module.css";
 
 const Header = () => {
+  const [{ theme, isDark }] = useContext(ThemeContext);
   return (
     <header>
       <div>
-        <h1 className={headerStyle.heading}> Social Media Dashboard</h1>
-        <strong className={headerStyle.totalFollowers}>
+        <h1
+          className={headerStyle.heading}
+          style={{ color: isDark ? theme.textWhiteColor : theme.textBlueColor }}
+        >
+          Social Media Dashboard
+        </h1>
+
+        <strong
+          className={headerStyle.totalFollowers}
+          style={{
+            color: isDark ? theme.textGrayishColor : theme.textBlueColor,
+          }}
+        >
           Total Followers: 23,004
         </strong>
       </div>
       <div className={headerStyle.darkMode}>
-        <h3>Dark Mode</h3>
+        <h3
+          style={{
+            color: isDark ? theme.textGrayishColor : theme.textBlueColor,
+          }}
+        >
+          Dark Mode
+        </h3>
         <Switch />
       </div>
     </header>
